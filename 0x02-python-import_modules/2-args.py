c737c453f17726b800726da1f2d843c850a20324
aquazodd <yohannesabiti309412@gmail.com>
2023-08-11 20:56:44 -0700

    updated third file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,13 +9,11 @@ if __name__ == "__main__":
         print("{} {}.".format(length - 1, str))
     elif length > 1:
         str = "argument"
-        print("{} {}.".format(length, str))
+        print("{} {}.".format(length - 1, str))
     elif length > 1:
         str = "arguments"
-        print("{} {} : \n".format(length, str))
+        print("{} {} : \n".format(length - 1, str))
 
     for z in range(1, length):
-        if z <= 1:
-            print("oops")
         print(sys.argv[z])
         print(z)
